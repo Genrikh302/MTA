74fe5c8eb09a490c86e0f10a742bd4a5aab91383
Maksim Kolesnikov <umkasuper@gmail.com>
2015-06-25 11:29:58 +0300

    Столбец типа и расскрасска

--- cdrtablemodel.cpp
+++ cdrtablemodel.cpp
@@ -4,6 +4,8 @@
 #include <QSqlQuery>
 #include <QDateTime>
 
+#include "qcallog.h"
+
 QCDRTableModel::QCDRTableModel() : QSqlTableModel()
 {
 
@@ -19,22 +21,23 @@ QVariant QCDRTableModel::data(const QModelIndex & index, int role) const
     if (!index.isValid())
         return QVariant();
 
-
-
     if (role == Qt::TextColorRole) { // расскрасим
-        QString intype =  QSqlTableModel::data(QSqlTableModel::index(index.row(), COL_TYPE_IN), Qt::DisplayRole).toString();
-        QString outtype =  QSqlTableModel::data(QSqlTableModel::index(index.row(), COL_TYPE_OUT), Qt::DisplayRole).toString();
-        if (intype == "C" && outtype == "C")
-            return QBrush(QColor(100, 100, 200));
-        if (intype == "C" && outtype == "A")
-            return QBrush(QColor(50, 255, 50));
-        if (intype == "A" && outtype == "C")
-            return QBrush(QColor(100, 200, 100));
+        int typecall = QSqlTableModel::data(QSqlTableModel::index(index.row(), COL_CALL_TYPE), Qt::DisplayRole).toInt();
+        switch (typecall) {
+            case Qcallog::TYPE_LOCAL: QBrush(QColor(255, 255, 255));;
+            case Qcallog::TYPE_IN_LOCAL: QBrush(QColor(50, 255, 50));
+            case Qcallog::TYPE_OUT_INTERNATIONAL: QBrush(QColor(20, 20, 20));;
+            case Qcallog::TYPE_OUT_NATIONAL: QBrush(QColor(20, 20, 20));;
+            case Qcallog::TYPE_OUT_LOCAL: QBrush(QColor(100, 200, 100));
+            case Qcallog::TYPE_TRANZIT_INTERNATIONAL: return QBrush(QColor(255, 80, 80));
+            case Qcallog::TYPE_TRANZIT_NATIONAL: return QBrush(QColor(255, 10, 20));
+            case Qcallog::TYPE_TRANZIT_LOCAL: return QBrush(QColor(100, 100, 200));
+        }
     }
 
 
     if (role == Qt::DisplayRole) {
-        if (index.column() == COL_TYPE_IN || index.column() == COL_TYPE_OUT) {
+        if (index.column() == COL_IN_TYPE || index.column() == COL_OUT_TYPE) {
             QString strtype =  QSqlTableModel::data(QSqlTableModel::index(index.row(), index.column() + 0), role).toString();
             QString str1 =  QSqlTableModel::data(QSqlTableModel::index(index.row(), index.column() + 1), role).toString();
             QString str2 = QString("%1").arg(QSqlTableModel::data(QSqlTableModel::index(index.row(), index.column() + 2), role).toInt(), 3, 10, QLatin1Char('0'));
@@ -44,14 +47,50 @@ QVariant QCDRTableModel::data(const QModelIndex & index, int role) const
             return val;
         }
 
-        if (index.column() == COL_DATE_OUT) {
-            QDate d = QDate::fromJulianDay(QSqlTableModel::data(QSqlTableModel::index(index.row(), index.column() + 0), role).toLongLong());
-            //qDebug() << QSqlTableModel::data(QSqlTableModel::index(index.row(), index.column() + 0), role).toLongLong();
-            //qDebug() << d.toString("dd-MM-yyyy");
-            return d.toString("dd-MM-yyyy");
-        }
+        if (index.column() == COL_DATE)
+            return QDate::fromJulianDay(QSqlTableModel::data(QSqlTableModel::index(index.row(), index.column() + 0), role).toLongLong()).toString("dd-MM-yyyy");
+
+        if (index.column() == COL_TIME)
+            return QTime(0, 0).addSecs(QSqlTableModel::data(QSqlTableModel::index(index.row(), index.column() + 0), role).toLongLong()).toString("hh-mm-ss");
+
+        if (index.column() == COL_TIME_SEIZ)
+            return QTime(0, 0).addSecs(QSqlTableModel::data(QSqlTableModel::index(index.row(), index.column() + 0), role).toLongLong()).toString("hh-mm-ss");
+
+        if (index.column() == COL_TIME_TALK)
+            return QTime(0, 0).addSecs(QSqlTableModel::data(QSqlTableModel::index(index.row(), index.column() + 0), role).toLongLong()).toString("hh-mm-ss");
+
+        if (index.column() == COL_CALL_TYPE)
+            return getQStringTypeCalls(QSqlTableModel::data(QSqlTableModel::index(index.row(), index.column() + 0), role).toInt());
 
     }
 
     return QSqlTableModel::data(index, role);
 }
+
+
+//int QCDRTableModel::getIntTypeCalls(const QModelIndex &index) const
+//{
+
+//    QStringList l = QStringList()
+//            << QSqlTableModel::data(QSqlTableModel::index(index.row(), COL_IN_TYPE), Qt::DisplayRole).toString()
+//            << QSqlTableModel::data(QSqlTableModel::index(index.row(), COL_OUT_TYPE), Qt::DisplayRole).toString()
+//            << QSqlTableModel::data(QSqlTableModel::index(index.row(), COL_IN_NUMBER), Qt::DisplayRole).toString()
+//            << QSqlTableModel::data(QSqlTableModel::index(index.row(), COL_OUT_NUMBER), Qt::DisplayRole).toString();
+//    return Qcallog::getIntTypeCalls(l);
+//}
+
+QString QCDRTableModel::getQStringTypeCalls(int value) const
+{
+    switch (value) {
+        case Qcallog::TYPE_LOCAL: return "Внутренний";
+        case Qcallog::TYPE_IN_LOCAL: return "Вход местный";
+        case Qcallog::TYPE_OUT_INTERNATIONAL: return "Исход международный";
+        case Qcallog::TYPE_OUT_NATIONAL: return "Исход междугородный";
+        case Qcallog::TYPE_OUT_LOCAL: return "Исход местный";
+        case Qcallog::TYPE_TRANZIT_INTERNATIONAL: return "Международный транзит";
+        case Qcallog::TYPE_TRANZIT_NATIONAL: return "Междугородный транзит";
+        case Qcallog::TYPE_TRANZIT_LOCAL: return "Транзит";
+        default: return "unkniwn";
+    }
+}
+
--- cdrtablemodel.h
+++ cdrtablemodel.h
@@ -8,19 +8,34 @@
 
 class QCDRTableModel : public QSqlTableModel
 {
+private:
+    int getIntTypeCalls(const QModelIndex &index) const; //
+    QString getQStringTypeCalls(int value) const;
+
+
 public:
     QCDRTableModel();
     QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
     ~QCDRTableModel();
 
-    const unsigned char COL_TYPE_IN    = 0;
-    const unsigned char COL_TYPE_OUT   = 6;
-    const unsigned char COL_MODULE_OUT = 7;
-    const unsigned char COL_PCM_OUT    = 8;
-    const unsigned char COL_PORT_OUT   = 9;
-    const unsigned char COL_NUMBER_OUT = 10;
-    const unsigned char COL_AON_OUT    = 11;
-    const unsigned char COL_DATE_OUT   = 12;
+    static const unsigned char COL_IN_TYPE    = 0;
+    static const unsigned char COL_IN_MN      = 1; // In module number
+    static const unsigned char COL_IN_SLPCM   = 2; // In slot/pcm number
+    static const unsigned char COL_IN_PORT    = 3; // In port number
+    static const unsigned char COL_IN_AON     = 4; // In AON number
+    static const unsigned char COL_IN_NUMBER  = 5; // In Number
+    static const unsigned char COL_OUT_TYPE   = 6;
+    static const unsigned char COL_OUT_MN     = 7;
+    static const unsigned char COL_OUT_SLPCM  = 8;
+    static const unsigned char COL_OUT_PORT   = 9;
+    static const unsigned char COL_OUT_AON    = 10;
+    static const unsigned char COL_OUT_NUMBER = 11;
+    static const unsigned char COL_DATE       = 12;
+    static const unsigned char COL_TIME       = 13;
+    static const unsigned char COL_CALL_TYPE  = 14;
+    static const unsigned char COL_TIME_SEIZ  = 15;
+    static const unsigned char COL_TIME_TALK  = 16;
+    static const unsigned char COL_CRELEASE   = 17;
 };
 
 #endif // MYTABLEMODEL_H
